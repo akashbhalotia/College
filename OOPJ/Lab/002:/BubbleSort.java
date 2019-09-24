4da9f7596a439e5713141e8571425c90a86e373d
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-09-24 13:45:37 +0530

    Create BubbleSort.java

---
+++ OOPJ/Lab/002:/BubbleSort.java
@@ -0,0 +1,63 @@
+/*
+   Sort an array using bubble sort.
+ */
+
+import java.util.Scanner;
+class Sorting
+{
+    private int N; //data members
+    private int[] a;
+
+    public Sorting() //constructor taking the input
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter array size");
+        N=sc.nextInt();
+        a=new int[N];
+
+        for(int i=0;i<N;i++)
+        {
+            System.out.println("Enter a number");
+            a[i]=sc.nextInt();
+        }
+    }
+    public void sort() //sorts the array
+    {
+        int tmp;
+
+        for(int i=0;i<N;i++)
+        {
+            for(int j=i;j<N-i-1;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    tmp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=tmp;
+                }
+            }
+        }
+    }
+    public void display() //displays the array
+    {
+        for(int i=0;i<N;i++)
+            System.out.print(a[i]+",");
+        System.out.println();
+    }
+}
+class Driver //driver class
+{
+    public static void main(String[] args)
+    {
+        Sorting obj=new Sorting();
+
+        System.out.println("Array before sorting:");
+        obj.display();
+
+        obj.sort();
+
+        System.out.println("Array after sorting:");
+        obj.display();
+    }
+}
