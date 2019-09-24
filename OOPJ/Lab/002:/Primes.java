6bf55213181bf342690b252a422a9efb8aa71958
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-09-24 13:30:45 +0530

    Create Primes.java

---
+++ OOPJ/Lab/002:/Primes.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Primes
+{
+    private int N; //data member
+
+    public Primes(int N) //parameterised constructor
+    {
+        this.N=N;
+    }
+    private boolean isPrime(int N) //checks if N is a prime number
+    {
+        int c=0;
+
+        for(int i=1;i<=N;i++)
+        {
+            if(N%i==0)
+                c++;
+        }
+        return (c==2);
+    }
+    public void printPrimes() //prints all primes till N (inclusive)
+    {
+        System.out.println("Primes till "+N+":");
+        for(int i=1;i<=N;i++)
+        {
+            if(isPrime(i))
+                System.out.println(i);
+        }
+    }
+}
+class Driver //driver class
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int N=sc.nextInt();
+        Primes obj=new Primes(N);
+        obj.printPrimes();
+    }
+}
