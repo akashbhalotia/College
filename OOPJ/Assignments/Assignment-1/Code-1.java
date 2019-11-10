e0a12a6ef3785740b01b2db47beb4e18a3134877
Akash Bhalotia <akashbhalotia10@gmail.com>
2019-11-10 16:35:02 +0530

    Create Code-1.java

---
+++ OOPJ/Assignments/Assignment-1/Code-1.java
@@ -0,0 +1,11 @@
+// created by Whiplash99
+import java.io.*;
+class Example
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int N=Integer.parseInt(br.readLine().trim());
+    }
+}
