a2a19750473dbf719f75f713a074f9d478161ac4
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-08-30 23:10:32 +0530

    Update InsertionSort.c

--- DAA/Homework/InsertionSort.c
+++ DAA/Homework/InsertionSort.c
@@ -6,54 +6,57 @@
 
 
 //created by Whiplash99
-#include<stdio.h>
+#include <stdio.h>
 
-void swap(int *a, int *b)
+void input(int a[], int N) //takes input
 {
-	int tmp=*a;
-	*a=*b;
-	*b=tmp;
+    for(int i=0;i<N;i++)
+    {
+        printf("Enter a number\n");
+        scanf("%d",&a[i]);
+    }
 }
-void sort(int a[], int N)
+void swap(int *a, int *b) //swaps the values of two numbers
 {
-	int i,j,tmp;
-	
-	for(i=1;i<N;i++)
-	{
-		j=i-1;
-		while(j>=0&&a[j]>a[j+1])
-		{	
-			 swap(&a[j],&a[j+1]);
-			 j--;
-		}
-	}
+    int tmp=*a;
+    *a=*b;
+    *b=tmp;
 }
-void display(int a[], int N)
+void sort(int a[], int N) //sorts the array
 {
-	int i;
-	for(i=0;i<N;i++) printf("%d ",a[i]);
-	printf("\n");
+    for(int i=0;i<N;i++)
+    {
+        int j=i;
+        while (j>0&&a[j]<a[j-1])
+        {
+            swap(&a[j],&a[j-1]);
+            j--;
+        }
+    }
 }
-int main()
+void display(int a[], int N) //displays elements of the array
 {
-	int i,N;
-	
-	printf("Enter the number of elements\n");
-	scanf("%d",&N);
-	
-	int a[N];
-	for(i=0;i<N;i++)
-	{
-		printf("Enter a number\n");
-		scanf("%d",&a[i]);
-	}
-	
-	printf("\nArray before sorting:\n");
-	display(a,N);
-	
-	sort(a,N);
-	printf("\nArray after sorting:\n");
-	display(a,N);
-	
-	return 0;
+    for(int i=0;i<N;i++)
+        printf("%d,",a[i]);
+    printf("\n\n");
+}
+int main() //main method
+{
+    int N;
+
+    printf("Enter the size of the array\n");
+    scanf("%d",&N);
+
+    int a[N];
+    input(a,N);
+
+    printf("\nArray before sorting:\n");
+    display(a,N);
+
+    sort(a,N);
+
+    printf("Array after sorting:\n");
+    display(a,N);
+    
+    return 0;
 }
