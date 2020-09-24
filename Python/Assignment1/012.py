833f26b47affc706c95b847684e67313762a3257
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-09-24 11:56:28 +0530

    Update 012.py

--- Python/Assignment1/012.py
+++ Python/Assignment1/012.py
@@ -1,5 +1,5 @@
 # WAP to sort a list of tuples by second item.
-# list1.sorted() modifies list1
+# list1.sort() modifies list1
 # sorted(list1) would have not modified it, but returned a sorted list.
 
 def second(ele):
