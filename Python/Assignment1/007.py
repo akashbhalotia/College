b25fec0264a0ad04f4fef9dd68cb84bd22983022
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-09-24 11:29:05 +0530

    Create 007.py

---
+++ Python/Assignment1/007.py
@@ -0,0 +1,19 @@
+# Write a program to find the Nth fibonacci number.
+
+def fibo(N):
+    if N == 1:
+        return 0
+    if N == 2:
+        return 1
+
+    a, b, c = 0, 1, 0
+
+    for i in range(N - 2):
+        c = a + b
+        a, b = b, c
+
+    return c
+
+
+val = 10
+print(fibo(val))
