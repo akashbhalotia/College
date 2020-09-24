547901ebd49d8babc7bfa49a987bd5909e790609
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-09-24 12:43:03 +0530

    Update 004.py

--- Python/Assignment1/004.py
+++ Python/Assignment1/004.py
@@ -1,9 +1,12 @@
 # WAP to print all negative numbers in a range.
+# end is a parameter inside print function.
+# It tells print what character to print, after printing the given ones.
+# By default, it's value is '\n', i.e., new line character.
 
-def printNeg(start, end):
-    for i in range(start, end+1):
+def printNeg(l, r):
+    for i in range(l, r + 1):
         if i < 0:
-            print(i)
+            print(i, end=" ")
 
 
 printNeg(-10, -5)
