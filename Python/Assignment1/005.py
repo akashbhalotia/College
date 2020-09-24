20acdc89ca34331ba78d8d8779555fa37faf16ae
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-09-24 10:56:57 +0530

    Create 005.py

---
+++ Python/Assignment1/005.py
@@ -0,0 +1,22 @@
+# WAP to print duplicates from a list of integers
+
+def findDuplicates(list1):
+    store = set()
+    duplicates = set()
+
+    for i in list1:
+        if i not in store:
+            store.add(i)
+        else:
+            duplicates.add(i)
+
+    return duplicates
+
+
+inputList = [1, 5, 2, 8, 2, 1, 9, 3, 10, 5, 1]
+print('Input list:')
+print(inputList)
+
+dupl = findDuplicates(inputList)
+print('\nDuplicates:')
+print(dupl)
