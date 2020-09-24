1acf7b1635f548f5feb039abc6ba27d33f8a29b6
Akash Bhalotia <akashbhalotia10@gmail.com>
2020-09-24 11:55:59 +0530

    Create 012.py

---
+++ Python/Assignment1/012.py
@@ -0,0 +1,17 @@
+# WAP to sort a list of tuples by second item.
+# list1.sorted() modifies list1
+# sorted(list1) would have not modified it, but returned a sorted list.
+
+def second(ele):
+    return ele[1]
+
+
+def sortTuples(list1):
+    list1.sort(key=second)
+
+
+inputList = [(1, 2), (1, 3), (3, 3), (0, 3), (3, 5), (7, 2), (5, 5)]
+print('Input List:', inputList)
+
+sortTuples(inputList)
+print('\nSorted:', inputList)
